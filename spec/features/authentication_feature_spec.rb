900c6e5925643e1067a03d573550eb4a58b3d138
Jack Nguyen <nguyen.jack@gmail.com>
2014-03-17 21:50:02 -0700

    create user sign in page

--- spec/features/authentication_feature_spec.rb
+++ spec/features/authentication_feature_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe 'user authentication' do
-  it 'allows sigin with valid credentials' do
+  it 'allows signin with valid credentials' do
     sign_user_in(create(:user))
 
     expect(page).to have_content('Signed in successfully')
   end
 
-  it 'does not allow sigin with invalid credentials' do
+  it 'does not allow signin with invalid credentials' do
     sign_user_in(create(:user), password: 'wrong pw')
 
     expect(page).to have_content('Invalid email or password')
@@ -26,6 +26,6 @@ end
 def sign_user_in(user, opts={})
   visit new_user_session_path
   fill_in 'Email', with: user.email
-  fill_in 'Password', with: user.password
+  fill_in 'Password', with: (opts[:password] || user.password)
   click_button 'Sign in'
 end
