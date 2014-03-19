f4da6cc4aea329ea91004a7bfae447da18eef98d
Jack Nguyen <nguyen.jack@gmail.com>
2014-03-18 21:35:26 -0700

    create user sign in with devise

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -4,18 +4,19 @@ module FormHelper
   end
 
     #takes the form that we're working with, the particular fields, and a block
-  def form_group_for(form, field, &block)
+  def form_group_for(form, field, opts={}, &block)
+    label = opts.fetch(:label) { true }
     #checks if the form field has errors
     has_errors = form.object.errors[field].present?
 
     #then it's going to create a div with the styles that bootstrap 3 expects
     content_tag :div, class: "form-group #{'has-error' if has_errors}" do
       #then it's going to create a lable for our field
-      concat form.label(field, class: 'control-label')
+      concat form.label(field, class: 'control-label') if label
       #output whatever is in the block, in this case it's going to be an input
       concat capture(&block)
       #then it's going to output any errors associated with that field
       concat errors_for(form, field)
     end
-  end
+  end 
 end
